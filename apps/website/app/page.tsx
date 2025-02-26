498c902c1914ee9370267cb8b70467554d6744f1
Robin Marsden <munkrat@gmail.com>
2025-02-26 16:37:48 +0000

    WIP clean up boilerplate

---
+++ apps/website/app/page.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@repo/ui/card";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
+        <h1>LWO website</h1>
+      </div>
+
+      <Card title="Shared UI - Card component">This is shared UI!</Card>
+    </main>
+  );
+}
